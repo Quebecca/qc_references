ec83f39c2cb86be26bc820109ddde053925a4cd9
Abderrazak Tgiti <tigitiabderrazak@gmail.com>
2022-04-06 16:51:44 +0000

    Add Build repo, fix cgl issus

--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,8 @@
 <?php
+
 defined('TYPO3_MODE') || die();
 
 use Qc\QcReferences\ReferencesReport;
-
 
 // Extend Module INFO with new Element
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
