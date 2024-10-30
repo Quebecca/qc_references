f8b090a980f3df8ecc335f7a7a82fa3159b0e6e4
Abderrazak Tigiti <abderrazak.tigiti@mce.gouv.qc.ca>
2024-10-30 16:54:03 +0100

    Fix user configuration

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -5,9 +5,8 @@ use Qc\QcReferences\Controller\ReferencesReport;
 return [
     'web_QcReferences' => [
         'parent' => 'web_info',
-        //'position' => ['after' => 'web_info'],
-        'access' => 'user,group',
-        'icon' => '',
+        'access' => 'user',
+        'iconIdentifier' => 'qc-references-backend-module-icon',
         'path' => '/module/web/info/QcReferences',
         'labels' => 'LLL:EXT:qc_references/Resources/Private/Language/locallang.xlf:mod_qcPageReferences',
         'extensionName' => 'QcReferences',
