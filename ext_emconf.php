60a0ceb8e0e17b25a2dd0a0e6eb60ab2241bd0e8
Abderrazak Tigiti <abderrazak.tigiti@mce.gouv.qc.ca>
2024-12-18 12:24:10 +0100

    Add ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext: "qc_references"
+ *
+ *
+ * Manual updates:
+ * Only the data in the array - anything else is removed by next write.
+ * "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Qc References',
+    'description' => "This module shows the references to the selected pages in the Pagetree, even if you don't have access to the content linking to it.",
+    'author' => 'Quebec.ca',
+    'category' => 'Module',
+    'state' => 'beta',
+    'version' => '2.0.0',
+    'autoload' => [
+        'psr-4' => [
+            'Qc\\QcReferences\\' => 'Classes',
+        ],
+    ],
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.9.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
